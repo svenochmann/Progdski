5883040871b952a5fa21102afbdb8471af7d9d18
SvenO <sven.och@outlook.com>
2022-11-06 14:40:58 +0100

    auf_2 Done 100%

--- uebung_4/Aufgabe_2.py
+++ uebung_4/Aufgabe_2.py
@@ -3,21 +3,27 @@
 def caesar_encode(text, shift, alphabet="abcdefghijklmnopqrstuvwxyz"):
     encrypt_text =""
     for letter in text:
-            
+            if letter in alphabet and " ":
+                position = alphabet.index(letter)
+                new_position = position + shift
+                if new_position >25:
+                    """
+                        Checks if the position is out of range 
+                        and if so it counts back from the beginning
+                        than saves it to my encrypt text
+                    """
+                    new_position_2 = new_position-26
         
-        position = alphabet.index(letter)
-        
-        new_position = position + shift
-        if new_position >25:
-            new_position_2 = new_position-26
+                    new_letter_2 = alphabet[new_position_2]
             
-            new_letter_2 = alphabet[new_position_2]
-            
-            encrypt_text += new_letter_2
-        else:
-            new_letter_1 = alphabet[new_position]
-            encrypt_text += new_letter_1
-    return encrypt_text
+                    encrypt_text += new_letter_2
+                else:
+                    new_letter_1 = alphabet[new_position]
+                    encrypt_text += new_letter_1
+            else:
+                encrypt_text += letter
+                pass    
+    return encrypt_text #  =>Gives back the new and encrypted text
 
 encrypted_text= "xri eztyk jtycvtyk wlvi uve rewrex."
 
@@ -27,6 +33,8 @@ for i in range(25):
         to find the right shift
     """
     print(caesar_encode(encrypted_text, i))
-
-
+"""
+    Das ergebnis ist:
+    "gar nicht schlecht fuer den anfang."   
     
+"""
