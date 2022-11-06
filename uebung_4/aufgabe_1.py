7ec262d44a49258868c20d66c8381043aba9a6bc
SvenO <sven.och@outlook.com>
2022-11-06 14:21:46 +0100

    Space problem solved

--- uebung_4/aufgabe_1.py
+++ uebung_4/aufgabe_1.py
@@ -1,27 +1,31 @@
 """Aufgabe 4.1"""
 
 
-
 def caesar_encode(text, shift, alphabet="abcdefghijklmnopqrstuvwxyz"):
     encrypt_text =""
-    for letter in text:    
-        if letter in alphabet:
-             """Es Checkt ob letter im alphabet ist"""
-             position = alphabet.index(letter)
-        else:
-            pass
-        new_position = position +shift
-        if new_position >25:
-            new_position_2 = new_position-26
+    
+    for letter in text:
+            if letter in alphabet and " ":
+                position = alphabet.index(letter)
+                new_position = position + shift
+                if new_position >25:
+                    """
+                        Checks if the position is out of range 
+                        and if so it counts back from the beginning
+                        than saves it to my encrypt text
+                    """
+                    new_position_2 = new_position-26
+        
+                    new_letter_2 = alphabet[new_position_2]
             
-            new_letter_2 = alphabet[new_position_2]
-            
-            encrypt_text += new_letter_2
-        else:
-            new_letter_1 = alphabet[new_position]
-            encrypt_text += new_letter_1
-    return encrypt_text
-
+                    encrypt_text += new_letter_2
+                else:
+                    new_letter_1 = alphabet[new_position]
+                    encrypt_text += new_letter_1
+            else:
+                encrypt_text += letter
+                pass    
+    return encrypt_text #  =>Gives back the new and encrypted text
 
 
 
