4febaa66397de44268964cdbbf9c5941eae9bfde
SvenO <sven.och@outlook.com>
2022-11-08 17:56:05 +0100

    Jetzt ist aufgabe 2 fertig

--- uebung_5/aufgabe_2.py
+++ uebung_5/aufgabe_2.py
@@ -1,4 +1,4 @@
-""""Aufgabe 2""
+""""Aufgabe 2"""
 
 class Person:
     
@@ -22,21 +22,9 @@ person2 = Person("Henry", 77)
 person3 = Person("Pieter", 17)
 
 
-
-
 person1.compare_age(person2) # => "Henry is the same age."
 person1.compare_age(person3)  # => "Pieter is younger than me."
 person3.compare_age(person1)  # => "Sally is older than me."
 
 
 
-
-"""
-print(type(person1))
-        if person1 == person2:
-            print(f"{person2} is the same age.")
-        elif person1 > person2:
-            print(f"{person2}is younger than me.")
-        else:
-            print(f"{person2} is older than me.")
-"""
