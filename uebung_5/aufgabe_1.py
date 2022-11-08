7440a4214b1c5eb3b3084335d75e65f04439e13b
SvenO <sven.och@outlook.com>
2022-11-08 16:22:12 +0100

    aufgabe 1 Done

--- uebung_5/aufgabe_1.py
+++ uebung_5/aufgabe_1.py
@@ -1,3 +1,8 @@
+"""Aufgabe 1"""
 
+class SuperHero:
+    def are_you_a_super_hero(someone):
+        print("No! I am just a regular person")
 
-
+someone = SuperHero()
+someone.are_you_a_super_hero()
