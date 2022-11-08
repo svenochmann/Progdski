2d40898465edb5c729ac3544ec055af08ea7990f
SvenO <sven.och@outlook.com>
2022-11-08 18:01:25 +0100

    Audgabe3 angefangen

---
+++ uebung_5/aufgabe_3.py
@@ -0,0 +1,19 @@
+"""Aufgabe 3"""
+
+
+class Circle:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+
+    
+        
+
+
+circle_1 = Circle(x=2, y=3.5, radius=5.5)
+print(circle_1.radius)  # --> 5.5
+
+circle_1.center.position()  # --> 2 3.5
+ 
