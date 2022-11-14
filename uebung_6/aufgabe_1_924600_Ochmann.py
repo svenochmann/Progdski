4bb054934b932d2d448280d80390209e9072ee52
Sven <sven.och@outlook.com>
2022-11-14 19:42:36 +0100

    prep 1

---
+++ uebung_6/aufgabe_1_924600_Ochmann.py
@@ -0,0 +1,23 @@
+"""Aufgabe 1"""
+
+import math
+class Point:
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y
+    
+    def __eq__(self, p):
+        return (self.x==p.x) and (self.y==p.y)
+    
+    def center_distance(self):
+        return math.sqrt(self.x**2+self.y**2)
+class Circle(Point):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius=radius
+
+
+"""Ausgabem Beispiel"""
+a=Circle(5, -3, 2.5)
+b=Circle(5, -3, 2.5)
+print(a==b)  # => Truea=Circle(5, -3, 2.5)b=Circle(5, -3, 25.0)print(a==b)  # => False
