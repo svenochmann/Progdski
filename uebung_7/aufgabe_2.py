bad927cbf9d8d78cae85cf43065b9a4cae222197
Sven Ochmann <sven.och@outlook.com>
2022-11-23 13:41:22 +0100

    aufgabe 2 pre

---
+++ uebung_7/aufgabe_2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def keep_cross(arr, n_row,n_col):
+
+
+    return None
+
+
+
+
+myarr=np.array([[1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12],
+                [13, 14, 15, 16]])
+
+
+print(keep_cross(my_arr, 2, 3))
