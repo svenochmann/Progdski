a1dd784f74aac023313ca46f0d7c94ab204c0954
Sven Ochmann <sven.och@outlook.com>
2022-11-23 13:36:02 +0100

    uebung7 auf 1 done

--- uebung_7/aufgabe_1.py
+++ uebung_7/aufgabe_1.py
@@ -1,6 +1,12 @@
-def create_square(size):
-    assert size >=2, "size must be >=2"   
+import numpy as np
 
+def create_square(size):
+    assert size >=2, "size must be >=2"  
+    #Creating a array with zeros
+    arr = np.zeros((size-2, size-2))
+    #on top of all zeros comes a 1
+    arr = np.pad(arr, pad_width=1, mode='constant', constant_values=1)
+    return arr 
 
 a=create_square(2)
 print(a)
@@ -17,3 +23,7 @@ print(a)
 #  [ 1.0.0.0.1.] 
 #  [ 1.0.0.0.1.]
 #  [ 1.1.1.1.1.]]
+
+"""
+Glaube nicht das die richtige lösung ist aber es geht
+"""
