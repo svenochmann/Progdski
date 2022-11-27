0ce4d849c493193a43fa5124491f9b59dd72c59d
SvenO <sven.och@outlook.com>
2022-11-27 20:07:57 +0100

    auf 7 done

--- uebung_7/aufgabe_2.py
+++ uebung_7/aufgabe_2.py
@@ -1,14 +1,20 @@
 import numpy as np
 
 def keep_cross(arr, n_row,n_col):
+    new_arr = np.zeros((4,4))
+    for i in range(4):
+        new_arr[i,n_row-1] = arr[i,n_row-1]
+    
+    for i in range(4):
+        new_arr[n_col-1,i] = arr[n_col-1,i]
+    
+     
+    return new_arr
 
 
-    return None
 
 
-
-
-myarr=np.array([[1, 2, 3, 4],
+my_arr=np.array([[1, 2, 3, 4],
                 [5, 6, 7, 8],
                 [9, 10, 11, 12],
                 [13, 14, 15, 16]])
